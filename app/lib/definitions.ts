fd38119b75048fa0c8f9f3ffb8851a2fe8d340eb
samueloliveira_m <trabalho.samueloliv@gmail.com>
2024-01-22 07:59:50 -0300

    templeite baixado

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -2,6 +2,9 @@
 // It describes the shape of the data, and what data type each property should accept.
 // For simplicity of teaching, we're manually defining these types.
 // However, these types are generated automatically if you're using an ORM such as Prisma.
+
+
+
 export type User = {
   id: string;
   name: string;
@@ -21,8 +24,6 @@ export type Invoice = {
   customer_id: string;
   amount: number;
   date: string;
-  // In TypeScript, this is called a string union type.
-  // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
   status: 'pending' | 'paid';
 };
 
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,16 +9,21 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
+import { useFormState } from 'react-dom';
 
 export default function EditInvoiceForm({
   invoice,
-  customers,
+  customers,  
 }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  return (
-    <form>
+  const initialState = { message: null, errors: {} };
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
+ 
+  return (<form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">

5bc1ea867da7cda9a5c74362914489d900367d34
samueloliveira_m <trabalho.samueloliv@gmail.com>
2024-01-23 21:06:45 -0300

    correção de bugs

--- app/dashboard/teacher/create/page.tsx
+++ app/dashboard/teacher/create/page.tsx
@@ -1,5 +1,5 @@
-import Form from '@/app/ui/invoices/create-form';
-import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import Form from '@/app/ui/teacher/create-form';
+import Breadcrumbs from '@/app/ui/teacher/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
  
 export default async function Page() {

77a8c6cde918e40ad9f6512712ebe1b0df08bfd5
samueloliveira_m <trabalho.samueloliv@gmail.com>
2024-03-27 16:47:23 -0300

    feat: Implementation component TeacherClassSchedule.tsx

--- app/dashboard/teacher/[id]/page.tsx
+++ app/dashboard/teacher/[id]/page.tsx
@@ -1,5 +1,5 @@
 import BannerTeacher from "@/app/ui/teacher/banner-teacher";
-import SubjectGradeGrid from "@/app/ui/teacher/subject-grade-grid";
+import SchoolClassCard from "@/app/ui/teacher/teacher-classses-cards";
 
 export default async function Page({ params }: { params: { id: string } }) {
   
@@ -10,7 +10,7 @@ export default async function Page({ params }: { params: { id: string } }) {
       <div className="border-b border-gray-300 mb-4">
         <BannerTeacher id={id}></BannerTeacher>
       </div>
-      <SubjectGradeGrid id={id}></SubjectGradeGrid>
+      <SchoolClassCard id={id}></SchoolClassCard>
     </>
   )  
 }

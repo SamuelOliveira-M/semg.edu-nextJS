db807b56851f4f096d84fe88f6bf4e525074e180
samueloliveira_m <trabalho.samueloliv@gmail.com>
2024-07-28 17:03:05 -0300

    fix: Redirection fix

--- app/ui/studant/create-form.tsx
+++ app/ui/studant/create-form.tsx
@@ -107,7 +107,7 @@ export default function Form({ studants,dataClass }: { studants: Studant[], data
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
-          href="/dashboard/class"
+          href={`/dashboard/class/${dataClass.id}`}
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
           Cancelar

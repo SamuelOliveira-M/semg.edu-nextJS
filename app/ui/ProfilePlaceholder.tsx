e9ed7c3c11baa756b00ada9b801f181bba6f3542
samueloliveira_m <trabalho.samueloliv@gmail.com>
2024-07-30 16:24:03 -0300

    feat: create profile picture template

---
+++ app/ui/ProfilePlaceholder.tsx
@@ -0,0 +1,8 @@
+
+export default function ProfilePlaceholder() {
+  return (
+    <div className="rounded-full bg-gray-200 w-10 h-10 flex items-center justify-center">
+       <p className='text-xs'> foto</p> 
+    </div>
+  );
+}

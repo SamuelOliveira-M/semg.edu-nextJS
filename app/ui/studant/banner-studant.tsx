989544e732cb8bc4ad5eb18d127546a3a12b25b2
samueloliveira_m <trabalho.samueloliv@gmail.com>
2024-08-01 17:04:26 -0300

    refactor: add new validation within the BannerStudant component

--- app/ui/studant/banner-studant.tsx
+++ app/ui/studant/banner-studant.tsx
@@ -1,15 +1,14 @@
 import { formatText } from "@/app/lib/utils"
 import { Suspense } from "react";
 import ProfilePicture from "@/app/ui/profile-picture";
-import { EnvelopeIcon } from "@heroicons/react/24/outline";
-import { RegistrationTable } from "@/app/lib/definitions";
+import { StudantTable } from "@/app/lib/definitions";
 import { formatCpf } from "@/app/lib/utils";
 import { LongProfilePlaceholder } from "../ProfilePlaceholder";
 
-export default async function BannerStudant({ profileDate }: {profileDate:RegistrationTable}){
+export default async function BannerStudant({ profileDate }: {profileDate:StudantTable}){
 
-	const img:string = profileDate.url_image
-	if(!img){
+	
+	if(!profileDate || !profileDate.url_image){
 		return (
 			<div className="pt-4 pb-4 pr-10 pl-10 mb-8 bg-blue-300 rounded-lg flex flex-col lg:flex-row justify-between items-center">
 				<div className="mb-4 lg:pt-0 text-center md:text-start">
@@ -30,7 +29,7 @@ export default async function BannerStudant({ profileDate }: {profileDate:Regist
 					<p className="text-sm">{formatCpf(profileDate.cpf)} </p>
 				</div>
 				<Suspense fallback={<p>....</p>}>
-					<ProfilePicture imageUrl={img}/>
+					<ProfilePicture imageUrl={profileDate.url_image}/>
 				</Suspense>
 			</div> 
 		</>
